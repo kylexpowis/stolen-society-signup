82208f07db9dd2223bef904a309369b8ec4e098f
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2024-07-12 11:32:13 +0100

    added logo and join for 20%

--- src/EmailForm.jsx
+++ src/EmailForm.jsx
@@ -1,27 +1,32 @@
-// src/EmailForm.jsx
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const EmailForm = () => {
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setMessage('Thank you for signing up!');
+    setMessage("Thank you for signing up!");
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col items-center w-full max-w-sm">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center w-full max-w-sm"
+    >
       <input
         type="email"
         placeholder="Enter your email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         required
-        className="p-2 mb-4 w-full text-black"
+        className="p-2 mb-4 w-full text-white"
       />
-      <button type="submit" className="p-2 w-full bg-white text-black hover:bg-gray-300">
-        Sign Up
+      <button
+        type="submit"
+        className="p-2 w-full bg-white text-black hover:bg-gray-300"
+      >
+        Submit
       </button>
       {message && <p className="mt-4">{message}</p>}
     </form>
@@ -29,4 +34,3 @@ const EmailForm = () => {
 };
 
 export default EmailForm;
-
--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import EmailForm from './EmailForm';
+import React from "react";
+import EmailForm from "./EmailForm";
+import { TypeAnimation } from "react-type-animation";
+import stolensocietylogo from "./assets/stolensocietylogo.png";
 
 const MainPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-full bg-black text-white">
-      <h1 className="text-4xl mb-8">Stolen Society</h1>
+      <img src={stolensocietylogo} alt="Logo" className=" h-52" />
+      <div className="mb-8">
+        <TypeAnimation
+          sequence={["Join the waiting list for 20% off", 1000]}
+          wrapper="span"
+          speed={20}
+          style={{
+            fontSize: "1em",
+            display: "inline-block",
+            fontFamily: "'Roboto', sans-serif",
+          }}
+          repeat={Infinity}
+        />
+      </div>
       <EmailForm />
     </div>
   );
