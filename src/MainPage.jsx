8c53e377aa69dc64f9b993790f5ff0d7a3aeaa42
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2024-07-14 20:54:22 +0100

    logo made bigger, email box smaller as per client request

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -3,15 +3,13 @@ import EmailForm from "./EmailForm";
 import { TypeAnimation } from "react-type-animation";
 import stolensocietylogo from "./assets/stolensocietylogo.png";
 
-
 const MainPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-full bg-black text-white">
-    
-      <img src={stolensocietylogo} alt="Logo" className=" h-52" />
+      <img src={stolensocietylogo} alt="Logo" className="h-[250px] w-auto" />
       <div className="mb-2">
         <TypeAnimation
-          sequence={["Join the waiting list for 20% off", 1000]}
+          sequence={["JOIN THE WAITING LIST FOR 20%", 1000]}
           wrapper="span"
           speed={20}
           style={{
@@ -23,7 +21,6 @@ const MainPage = () => {
         />
       </div>
       <EmailForm />
-      
     </div>
   );
 };
