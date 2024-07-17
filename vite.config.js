e497beaacb62119bdf91f08c564be89f5a464b55
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2024-07-17 16:32:07 +0100

    vite config link update

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,23 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import vercel from 'vite-plugin-vercel'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import vercel from "vite-plugin-vercel";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  const REGION = env.VITE_MAILCHIMP_REGION
-  const LIST_ID = env.VITE_MAILCHIMP_LIST_ID
+  const env = loadEnv(mode, process.cwd(), "");
+  const REGION = env.VITE_MAILCHIMP_REGION;
+  const LIST_ID = env.VITE_MAILCHIMP_LIST_ID;
 
   return {
-    plugins: [
-      react(),
-      vercel()
-    ],
+    plugins: [react(), vercel()],
     server: {
       proxy: {
-        '/api/mailchimp': {
+        "/api/mailchimp/members": {
           target: `https://${REGION}.api.mailchimp.com/3.0`,
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api\/mailchimp/, `/lists/${LIST_ID}`),
+          rewrite: (path) =>
+            path.replace(/^\/api\/mailchimp/, `/lists/${LIST_ID}`),
         },
       },
     },
-  }
-})
+  };
+});
