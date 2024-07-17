8cc379a9d78a0f17a9a9f45d9906a325de0c16a0
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2024-07-17 16:48:39 +0100

    changed import env to vite version

--- api/mailchimp.js
+++ api/mailchimp.js
@@ -1,28 +1,31 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 export default async function handler(req, res) {
   const { email } = req.body;
 
-  const REGION = process.env.VITE_MAILCHIMP_REGION;
-  const LIST_ID = process.env.VITE_MAILCHIMP_LIST_ID;
-  const API_KEY = process.env.MAILCHIMP_API_KEY;
+  const REGION = import.meta.env.VITE_MAILCHIMP_REGION;
+  const LIST_ID = import.meta.env.VITE_MAILCHIMP_LIST_ID;
+  const API_KEY = import.meta.env.MAILCHIMP_API_KEY;
 
   const data = {
     email_address: email,
-    status: 'subscribed',
+    status: "subscribed",
   };
 
-  const response = await fetch(`https://${REGION}.api.mailchimp.com/3.0/lists/${LIST_ID}/members`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `apikey ${API_KEY}`,
-    },
-    body: JSON.stringify(data),
-  });
+  const response = await fetch(
+    `https://${REGION}.api.mailchimp.com/3.0/lists/${LIST_ID}/members`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `apikey ${API_KEY}`,
+      },
+      body: JSON.stringify(data),
+    }
+  );
 
   if (response.ok) {
-    res.status(200).json({ message: 'Subscription successful' });
+    res.status(200).json({ message: "Subscription successful" });
   } else {
     const errorData = await response.json();
     res.status(response.status).json({ error: errorData });
