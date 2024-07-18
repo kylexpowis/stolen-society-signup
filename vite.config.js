4a93a8e5cf1356853cf26027e4faf65bf9ce64a2
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2024-07-18 13:40:18 +0100

    add netlify function and change addsubs

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import vercel from "vite-plugin-vercel";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  const REGION = env.VITE_MAILCHIMP_REGION;
-  const LIST_ID = env.VITE_MAILCHIMP_LIST_ID;
-
-  return {
-    plugins: [react(), vercel()],
-    server: {
-      proxy: {
-        "/api/mailchimp": {
-          target: `https://${REGION}.api.mailchimp.com/3.0`,
-          changeOrigin: true,
-          rewrite: (path) =>
-            path.replace(/^\/api\/mailchimp/, `/lists/${LIST_ID}`),
-        },
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
 });
